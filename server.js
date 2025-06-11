15824dc17d5951503b8091e359d569032308b543
s2500141 <s2500141@edu.bc.fi>
2025-06-11 12:52:02 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import jsonServer from 'json-server';
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use('/', router);
+
+server.listen(process.env.PORT || 3006, () => {
+    console.log('JSON Server is running');
+});
